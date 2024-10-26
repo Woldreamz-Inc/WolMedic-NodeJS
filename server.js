28c0b4a78d2f99addf6e06491898b6e37aaa13bb
Nyquist Nwaukwa <nyquist.nwaukwa@gmail.com>
2024-10-26 12:46:51 +0100

    Test

--- server.js
+++ server.js
@@ -13,11 +13,7 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,7 +29,7 @@ app.use("/api/equipment", equipmentRoutes);
 // Setup Swagger
 setupSwagger(app);
 
-sequelize
+/* sequelize
   .sync()
   .then(() => {
     console.log("Database connected and synced successfully");
@@ -44,4 +40,6 @@ sequelize
   })
   .catch((err) => {
     console.error("Unable to connect to the database:", err);
-  });
+  }); */
+
+module.exports = app;
