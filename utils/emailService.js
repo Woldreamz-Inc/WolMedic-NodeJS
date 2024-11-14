630f04c2fb4ea1c95408dbeb4e2ccd44eb0bf56f
Kooffreh98 <kooffreh2015@gmail.com>
2024-11-14 12:51:19 +0100

    changed verification email link

--- utils/emailService.js
+++ utils/emailService.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
 
 // Send Verification Email
 exports.sendVerificationEmail = async (email, token) => {
-  const verificationLink = `http://your-app.com/verify/${token}`;
+  const verificationLink = `https://medequip-api.vercel.app/api/auth/verify/${token}`;
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
